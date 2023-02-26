a6fbe2f9f17e534598a0ef674f7e256bf03bd7a2
Camilla-Lima <126366940+Camilla-Lima@users.noreply.github.com>
2023-02-25 23:55:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorldApp {
+
+    class Geeks {
+
+        static void Main(string[] args) {
+
+            double fahreinheit = 0;
+            double celcius = 0;
+
+        Console.WriteLine("Conversor de temperatura de Fahreinheit para Celcius");
+
+            Console.WriteLine("Insira a temperatura em Fahreinheit: ");
+            fahreinheit = Convert.ToDouble(Console.ReadLine());
+
+            celcius = (fahreinheit - 32.0) * (5.0 / 9.0);
+
+            Console.WriteLine("Sua temperatura em Celcius: " + celcius.ToString());
+            Console.ReadLine();
+        
+
+
+            
+        }
+    }
+}
